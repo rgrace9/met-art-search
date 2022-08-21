e20a736dc5683b00993d07c6b4d65649a4a15aea
Rebecca Williams <rgracewilliams@gmail.com>
2022-08-21 07:05:08 -0400

    Style the search bar.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,6 @@ import React, {useState} from 'react'
 
 function App() {
 
-  const [advancedSearch, setAdvancedSearch] = useState(false);
-
 
   // on refresh change the image and show in the bottom right corner show a hyperlink to the artist and title.
   // the user can click on the link to get to the artist's page
@@ -13,6 +11,12 @@ function App() {
     <div className="container">
        <div className='left-half'>
         <h1>Search the collection of New York's Metropolitan Museum of Art</h1>
+        <form className='home-form'>
+          <div className='search-bar-wrapper'>
+            <SearchIcon />
+            <input className='search-bar' placeholder='Search...'/>
+          </div>
+        </form>
        </div>
        <div className='right-half'>
         <div className='right-corner'>
